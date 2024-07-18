12ab5bd6c00cf18a36da122647d658c073070414
Your Name <you@example.com>
2024-07-18 15:22:32 +0800

    FINAL REVISE NI KENENH

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
               </a>
               <a
                 className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 text-gray-600 dark:text-gray-400 rounded-lg"
-                href="#"
+                href="Form"
               >
                 <FaShoppingCart className="h-6 w-6" />
                 <span className="mx-4 text-lg font-normal">Commerce</span>
